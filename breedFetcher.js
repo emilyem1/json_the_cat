d0a9f3d792d6b7744a7f73e8e6ad20c502499bc5
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-17 20:32:41 -0700

    adding command line to get request, and edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,22 @@
 const request = require('request'); // import request module
+const breedName = process.argv[2]; // get breed from command line
 
-// Simple get request
-request.get('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  if (error) { 
-    console.error('Error:', error);
-} else {
-    const data = JSON.parse(body); // turn json string into object
-    console.log(data); // now an object
-    console.log(typeof data); // confirms object 
+if (!breedName) { // if nothing is input into command line
+  console.log('Please provide a breed name as a command-line argument.');
+  process.exit(); // Exit so it doesn't also print line 16
 }
+
+// Get request
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => { // make the search breedName
+  const data = JSON.parse(body); // turn json string into object
+  if (error) {
+    console.error('Error:', error);
+  }
+  if (data.length === 0) { // if there is no data found on breed
+    console.log("Breed not found, please enter valid breed name.");
+  } else {
+    console.log(data[0].description); // logs description of specific cat
+  }
 });
 
 
