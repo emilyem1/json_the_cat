c3b2c6b8088ae101aac92b9e8594dd2a64e78782
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-17 23:09:28 -0700

    removing console.logs and replacing with callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,35 +1,21 @@
 const request = require('request'); // import request module
-const breedName = process.argv[2]; // get breed from command line
-// turn direct url into 'url' to clean up code 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
-
-if (!breedName) { // if nothing is input into command line
-  console.log('Please provide a breed name as a command-line argument.');
-  process.exit(); // Exit so it doesn't also print line 17
-};
 
 const fetchBreedDescription = function(breedName, callback) {
-    request.get(url, (error, response, body) => { 
+  // callback = (error, description)
+  request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => { // search for command line breedName
     if (error) {
-      console.log('Error', error)
+      callback(error, null); // description = null
     } else {
       const data = JSON.parse(body); // turn json string into object
-      if (data.length === 0) { // if no data is found 
-        console.log("Breed not found, please enter valid breed name.");
+      if (data.length === 0) { // if no data is found
+        callback(null, "Breed not found, please enter valid breed name.");
+        // error = null
       } else {
-          console.log(data[0].description); // logs description of specific cat
+        callback(null, data[0].description); // logs description of specific cat
+        // error = null
       }
     }
   });
 };
-
-// Call the function using breedName and then defining a callback function
-fetchBreedDescription(breedName, (error, description) => {
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    console.log(description);
-  }
-});
 
 module.exports = { fetchBreedDescription };
