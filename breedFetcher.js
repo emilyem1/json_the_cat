4a21ae70aee8106a589d1444b6b33817eb72dd2f
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-17 19:54:41 -0700

    making a get request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,3 +1,13 @@
 const request = require('request'); // import request module
 
-console.log(request); // see if it works 
+// Simple get request
+request.get('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  if (error) { 
+    console.error('Error:', error);
+} else {
+    console.log('Body:', body); // object containing details of sib cat
+    console.log(typeof body) // it's a string
+}
+});
+
+
