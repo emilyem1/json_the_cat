12b7e977107de987a630714db92cd608e5b7abca
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-17 21:45:05 -0700

    refactoring into fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,35 @@
 const request = require('request'); // import request module
 const breedName = process.argv[2]; // get breed from command line
+// turn direct url into 'url' to clean up code 
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
 if (!breedName) { // if nothing is input into command line
   console.log('Please provide a breed name as a command-line argument.');
-  process.exit(); // Exit so it doesn't also print line 16
-}
+  process.exit(); // Exit so it doesn't also print line 17
+};
 
-// Get request
-request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => { // make the search breedName
-  const data = JSON.parse(body); // turn json string into object
+const fetchBreedDescription = function(breedName, callback) {
+    request.get(url, (error, response, body) => { 
+    if (error) {
+      console.log('Error', error)
+    } else {
+      const data = JSON.parse(body); // turn json string into object
+      if (data.length === 0) { // if no data is found 
+        console.log("Breed not found, please enter valid breed name.");
+      } else {
+          console.log(data[0].description); // logs description of specific cat
+      }
+    }
+  });
+};
+
+// Call the function using breedName and then defining a callback function
+fetchBreedDescription(breedName, (error, description) => {
   if (error) {
-    console.error('Error:', error);
-  }
-  if (data.length === 0) { // if there is no data found on breed
-    console.log("Breed not found, please enter valid breed name.");
+    console.log('Error:', error);
   } else {
-    console.log(data[0].description); // logs description of specific cat
+    console.log(description);
   }
 });
 
-
+module.exports = { fetchBreedDescription };
